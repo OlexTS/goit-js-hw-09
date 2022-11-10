6b77311d88cf4d1a263790d9d0faef3bd6ce4d77
OlexTS <107061589+OlexTS@users.noreply.github.com>
2022-11-10 16:39:05 +0000

    Deploying to gh-pages from @ OlexTS/goit-js-hw-09@0bac46732e010fac0e4b07c6ae582e1037314063 🚀

---
+++ 01-color-switcher.28c3f4b3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.setAttribute("disabled",!0),timerId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.28c3f4b3.js.map
