0bac46732e010fac0e4b07c6ae582e1037314063
OlexTS <sashokseven@gmail.com>
2022-11-10 18:38:34 +0200

    made first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
 
+const btnStartRef = document.querySelector('[data-start]');
+const btnStopRef = document.querySelector('[data-stop]');
+
+btnStartRef.addEventListener('click', onBtnStart);
+btnStopRef.addEventListener('click', onBtnStop);
+
+function onBtnStart() {
+    btnStartRef.setAttribute('disabled', true);
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+    
+}
+
+function onBtnStop() {
+    btnStartRef.removeAttribute('disabled');
+    clearInterval(timerId);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
