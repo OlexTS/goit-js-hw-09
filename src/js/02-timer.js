95ab064ebfe32b4da257be9711e3deb4d4377f42
OlexTS <sashokseven@gmail.com>
2022-11-15 00:43:34 +0200

    made task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,26 +1,69 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
 const inputRef = document.querySelector('#datetime-picker');
 const buttonStartRef = document.querySelector('[data-start]');
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
+buttonStartRef.disabled = true;
 
-buttonStartRef.addEventListener('click', onBtnClick);
-buttonStartRef.setAttribute('disabled', true);
-
-flatpickr(inputRef, {
-    onClose(selectedDates) {
-       const time = selectedDates[0];
-        if (time < new Date()) {
-        window.alert("Please choose a date in the future")
-        }
-        else {time > new Date()
-            buttonStartRef.removeAttribute('disabled');
-           
-        }
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const chooseDate = Date.parse(selectedDates[0]);
+    if (chooseDate <= Date.parse(new Date())) {
+      Notify.failure('Please choose a date in the future');
+      return;
     }
-})
 
- function onBtnClick() {
-   console.log(time);
+    buttonStartRef.disabled = false;
+    buttonStartRef.addEventListener('click', () => {
+      buttonStartRef.disabled = true;
+      inputRef.disabled = true;
+      let timerId = null;
+      timerId = setInterval(() => {
+        let timeValue = convertMs(chooseDate - Date.parse(new Date()));
+        daysRef.textContent = addLeadingZero(timeValue.days);
+        hoursRef.textContent = addLeadingZero(timeValue.hours);
+        minutesRef.textContent = addLeadingZero(timeValue.minutes);
+        secondsRef.textContent = addLeadingZero(timeValue.seconds);
+
+        if (chooseDate - Date.parse(new Date()) === 0) {
+          clearInterval(timerId);
+          Notify.success('Time is up');
+        }
+      }, 1000);
+    });
+  },
+};
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
 }
-console.log('hello')
+
+const flatpickrEl = flatpickr(inputRef, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
